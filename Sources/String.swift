5eceeb55f1ee0b57edb18c85f40a5afbada4cc79
Jooyoung Park <smtown.ios.dev3@gmail.com>
2019-09-19 16:28:34 +0900

    Update String.swift

--- Sources/String.swift
+++ Sources/String.swift
@@ -8,10 +8,61 @@
 
 import Foundation
 
-extension String {
+public extension String {
+
+//MARK: String transformation
     
-    func trim() -> String {
+    /**
+     trim
+     */
+    public func trim() -> String {
         return self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
     }
     
+//MARK: Index
+    
+    /**
+     indexOf
+     */
+    public func indexOf(_ target: String) -> Int {
+        let range = self.range(of: target)
+        if let range = range {
+            return distance(from: self.startIndex, to: range.lowerBound)
+        } else {
+            return -1
+        }
+    }
+    
+    /**
+     lastIndexOf
+     */
+    public func lastIndexOf(target: String) -> Int? {
+        let range = (self as NSString).range(of: target, options: NSString.CompareOptions.backwards)
+        guard range.toRange() != nil else {
+            return nil
+        }
+        return self.length - range.location - 1
+    }
+
+//MARK: String check
+    
+    /**
+     Determines whether String has a specific string.
+     - parameters:
+        - s: specific string
+     */
+    public func contains(s: String) -> Bool {
+        return (self.range(of: s) != nil) ? true : false
+    }
+    
+//MARK: Localized String
+    
+    /**
+     Load NSLocalizedString
+     - parameters:
+        - key: Localizable.strings에 명시된 key
+     */
+    public func LocalizedString(_ key: String) -> String {
+        return NSLocalizedString(key, comment: "")
+    }
 }
